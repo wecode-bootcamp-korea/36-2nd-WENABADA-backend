5aba87968b2586ba56585c830beb9cebf395b1ad
JJ <jjmoon4682@gmial.com>
2022-08-29 18:08:54 +0900

    초기세팅 수정완료

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const { database } = require("./models/database.js");
 const startServer = async () => {
     const app = createApp();
     const PORT = process.env.PORT;
-    await database.initialize();
+    await database.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+        database.destroy()
+    });
     app.listen(PORT, () => {
         console.log(`Listening on Port ${PORT}`);
     });
