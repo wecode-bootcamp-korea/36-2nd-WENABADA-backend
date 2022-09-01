1dd7bac031e35f34963c9fef094e3e5430f2e3de
JeongJaeHa <jaeha2324@gmail.com>
2022-09-01 12:47:41 +0900

    category  API 구현, FE통신 완료, conflict 해결

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,44 +1,44 @@
 const categoryService = require('../services/categoryService');
 
 const getFirstCategoryProductList = async (req, res) => {
-    const  { firstCategory, pageNo, Limit }  = req.query;
-    const getFirstCategoryProductList = await categoryService.getFirstCategoryProductList( firstCategory, pageNo, Limit );
+    const  { firstCategory, pageNo, limit }  = req.query;
+    const getFirstCategoryProductList = await categoryService.getFirstCategoryProductList( firstCategory, pageNo, limit );
     res.status(200).json(getFirstCategoryProductList);
 }
 
 const getSubCategoryProductList = async (req, res) => {
-    const { firstCategory, subCategory, pageNo, Limit  }  = req.query;
-    const getSubCategoryProductList = await categoryService.getSubCategoryProductList( firstCategory, subCategory, pageNo, Limit  );
+    const { firstCategory, subCategory, pageNo, limit  }  = req.query;
+    const getSubCategoryProductList = await categoryService.getSubCategoryProductList( firstCategory, subCategory, pageNo, limit  );
     res.status(200).json(getSubCategoryProductList);
 }
 
 const getLastCategoryProductList = async (req, res) => {
-    const { firstCategory, subCategory, lastCategory, pageNo, Limit  }  = req.query;
-    const getLastCategoryProductList = await categoryService.getLastCategoryProductList(firstCategory, subCategory, lastCategory, pageNo, Limit );
+    const { firstCategory, subCategory, lastCategory, pageNo, limit  }  = req.query;
+    const getLastCategoryProductList = await categoryService.getLastCategoryProductList(firstCategory, subCategory, lastCategory, pageNo, limit );
     res.status(200).json(getLastCategoryProductList);
 }
 
 const getPriceFilterList = async (req,res) => {
-    const { firstCategory, subCategory, lastCategory, pageNo, Limit, option  } = req.query;
-    const getPriceFilter = await categoryService.getPriceFilterList(firstCategory, subCategory, lastCategory, pageNo, Limit, option );
+    const { firstCategory, subCategory, lastCategory, pageNo, limit, option  } = req.query;
+    const getPriceFilter = await categoryService.getPriceFilterList(firstCategory, subCategory, lastCategory, pageNo, limit, option );
     res.status(200).json(getPriceFilter);
 }
 
 const getNewDateList = async (req,res) => {
-    const { firstCategory, subCategory, lastCategory, pageNo, Limit, option  } = req.query;
-    const getNewDateList = await categoryService.getNewDateList(firstCategory, subCategory, lastCategory, pageNo, Limit, option );
+    const { firstCategory, subCategory, lastCategory, pageNo, limit, option  } = req.query;
+    const getNewDateList = await categoryService.getNewDateList(firstCategory, subCategory, lastCategory, pageNo, limit, option );
     res.status(200).json(getNewDateList);
 }
 
 const getFirstCategoryPriceFilterList = async (req,res) => {
-    const { firstCategory, pageNo, Limit, option  } = req.query;
-    const getFirstCategoryPriceFilterList = await categoryService.getFirstCategoryPriceFilterList(firstCategory, pageNo, Limit, option );  
+    const { firstCategory, pageNo, limit, option  } = req.query;
+    const getFirstCategoryPriceFilterList = await categoryService.getFirstCategoryPriceFilterList(firstCategory, pageNo, limit, option );  
     res.status(200).json(getFirstCategoryPriceFilterList);
 }
 
 const getFirstCategoryNewList = async (req,res) => {
-    const { firstCategory, pageNo, Limit, option  } = req.query;
-    const getFirstCategoryNewList = await categoryService.getFirstCategoryNewList(firstCategory, pageNo, Limit, option );  
+    const { firstCategory, pageNo, limit, option  } = req.query;
+    const getFirstCategoryNewList = await categoryService.getFirstCategoryNewList(firstCategory, pageNo, limit, option );  
     res.status(200).json(getFirstCategoryNewList);
 }
 
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.use("/users", userRouter.router);
 
 router.use('/category', categoryRouter.router);
 
+router.use('/category', categoryRouter.router);
+
 router.use('/products', productRouter.router);
 
 module.exports = router;
