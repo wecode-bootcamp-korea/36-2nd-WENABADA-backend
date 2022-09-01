b06a4b36a4d6991e64b67a1c4a94d442e75ed5eb
JJ <jjmoon4682@gmial.com>
2022-09-01 12:38:48 +0900

    login API 완료

---
+++ services/userService.js
@@ -0,0 +1,34 @@
+const userDao = require('../models/userDao')
+const express = require('express');
+const router = express.Router();
+const error = require("../middlewares/errorConstructor");
+const axios = require('axios')
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRETKEY; 
+
+const login = async (kakaoToken) => {
+    let user;
+    user = await axios({
+        method:"GET",
+        url:'https://kapi.kakao.com/v2/user/me',
+        headers:{
+            Authorization:`Bearer ${kakaoToken}`
+        }
+    })
+    const userId = user.data.id
+    if(!userId){
+        throw new error("KEY_ERROR", 400);
+    }
+    const mainId = await userDao.checkMainId(userId)
+    const payLoad = { id : mainId, social_id : userId }
+    const token = jwt.sign(payLoad, secretKey)
+    const idCheck = await userDao.idCheck(userId)
+    if(Number(Object.values(idCheck[0])[0]) === 0){
+        await userDao.createUser(userId)
+    }
+    return token
+};
+  
+module.exports = {
+    login, router
+}
