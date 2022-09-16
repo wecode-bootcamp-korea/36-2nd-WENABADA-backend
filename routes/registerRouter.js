7c0b19a6a58e58645d409fd8b9a8f7e9e86c3845
JaeHaaaa <99805929+JeongJaeHa@users.noreply.github.com>
2022-09-16 11:31:07 +0900

    Register Error Refactorig (#1)

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const  upload  = require('../register');
 const registerController = require('../controllers/registerController');
+const errorHandler = require("../middlewares/errorHandler");
 
-router.post('/upload', upload.single('img'), registerController.register);
+router.post('/upload', upload.single('img'), errorHandler(registerController.register));
 
 module.exports = {router};
--- services/registerService.js
+++ services/registerService.js
@@ -1,7 +1,11 @@
 const registerDao = require('../models/registerDao');
+const eNum = require("../models/eNum");
 
-const productRegister = async (title, userId, price, description, address, img, lastCategory ) => {
-  let address_id = await registerDao.selectLocation(address)
+const productRegister = async ( title, userId, price, description, address, img, lastCategory ) => {    
+  const address_id = await registerDao.selectLocation(address)
+  if(address_id.length == 0) {
+    throw new error('ADDRESS_NOT_CORRECT', 400)
+  }
   let location = await Number(Object.values(address_id[0])[0]);
   let local = location.toString();
   let category = lastCategory.toString();
